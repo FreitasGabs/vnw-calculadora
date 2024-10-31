c9dc180fac35d1cce08611525b3246a0ff6ad83a
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-10-31 19:38:50 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Calculadora simples
+Uma calculadora que pega dois números e realiza as 4 operações básicas (soma, subtração, multiplicação e divisão).
+
+## Tecnologias utilizadas:
+HTML, SCSS (CSS), JavaScript, e tudo isso por meio do React.
+
+## Detalhes:
+Treinando criações de Arrow Function, e ativações de eventos como o onChange e onClick.
+O projeto poderia ter sido criado de forma componentizada, mas terminei fazendo o deploy de forma rápida para sair ainda no Halloween.
+
+## Deploy:
+Você pode ver o projeto funcionando em:
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./global.scss";
+
+export default function App() {
+  const [primeiroValor, setPrimeiroValor] = useState();
+
+  const [segundoValor, setSegundoValor] = useState();
+
+  // criando variáveis para guardar os resultados
+  const [resultado, setResultado] = useState();
+
+  // adicionar (e)/(event) para marcar o evento do onChange
+  // o e é apenas o onChange, ainda não capturou os dados
+  // o .target.value extrai apenas o valor
+  // o onChange sempre pega como string, independente do tipo de valor na tag
+  const capturandoPrimeiroValor = (e) => {
+    setPrimeiroValor(Number(e.target.value));
+  };
+
+  const capturandoSegundoValor = (e) => {
+    setSegundoValor(Number(e.target.value));
+  };
+
+  const soma = () => {
+    setResultado(primeiroValor + segundoValor);
+  };
+
+  const subtracao = () => {
+    setResultado(primeiroValor - segundoValor);
+  };
+
+  const multi = () => {
+    setResultado(primeiroValor * segundoValor);
+  };
+
+  const divisao = () => {
+    setResultado(primeiroValor / segundoValor);
+  };
+  return (
+    <main>
+      <h1>Calculadora</h1>
+      {/* podemos tirar o type="number" pra tirar o menuzinho de subir e descer o número, e adicionar na lógica que os dados são números */}
+      {/* onChange é um evento de mudança (assim como o onClick era um evento de clique) */}
+      <input
+        type="number"
+        placeholder="Insira um número"
+        onChange={capturandoPrimeiroValor}
+      />
+      <input
+        type="number"
+        placeholder="Insira um número"
+        onChange={capturandoSegundoValor}
+      />
+
+      {/* criando os botões de operações */}
+      <section>
+        <button onClick={soma}>+</button>
+        <button onClick={subtracao}>-</button>
+        <button onClick={multi}>x</button>
+        <button onClick={divisao}>/</button>
+      </section>
+
+      {/* chamando o resultado */}
+      <h3>Resultado: {resultado}</h3>
+    </main>
+  );
+}
